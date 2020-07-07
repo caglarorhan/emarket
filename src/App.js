a455d50a6f427ef3f3cedee98808cd5dbef0469a
caglarorhan <caglaror@gmail.com>
2020-07-06 22:01:47 -0500

    Update menu items, set Route and History+Link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom'
+
+
 import './App.css';
 
-import HomePage from "./pages/homepage/homepage.components";
-
+const HomePage = ()=>(
+    <div>
+        <Link to='/topics'>Topics</Link>
+        <h1>HOME PAGE</h1>
+    </div>
+)
+const TopicList = ()=>(
+    <div>
+        <h1>TOPIC LIST PAGE</h1>
+    </div>
+)
+const TopicDetail = ()=>(
+    <div>
+        <h1>TOPIC DETAIL PAGE</h1>
+    </div>
+)
 
 function App() {
   return (
     <div>
-        <HomePage/>
+        <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route exact path='/topics' component={TopicList}/>
+            <Route exact path='/topics/:topicId' component={TopicDetail}/>
+        </Switch>
     </div>
   );
 }
