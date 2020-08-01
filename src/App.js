e7faff8807de3991783f4b8c6f2251975f1ae90e
caglarorhan <caglaror@gmail.com>
2020-08-01 03:29:14 -0500

    Checkout page added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.components";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import Header from "./components/header/header.component";
+
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {setCurrentUser} from "./redux/user/user.action";
 import {selectCurrentUser} from "./redux/user/user.selector";
@@ -51,6 +54,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
+                    <Route path='/checkout' component={CheckoutPage}/>
                     <Route exact path='/signin' render={() =>
                         this.props.currentUser ? (
                             <Redirect to='/'/>
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,10 +5,11 @@ import CustomButton from "../custom-button/custom-button.components";
 import CartItem from '../cart-item/cart-item.component';
 import {selectCartItems} from '../../redux/cart/cart.selector';
 import {createStructuredSelector} from "reselect";
-
+import {withRouter} from 'react-router-dom';
+import {toggleCartHidden} from '../../redux/cart/cart.action';
 import './cart-dropdown.style.scss';
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className="cart-items">
             {
@@ -20,7 +21,12 @@ const CartDropdown = ({cartItems}) => (
                     <span className='empty-message'>Cart is Empty</span>
             }
         </div>
-        <CustomButton> GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={
+            ()=> {
+            history.push('/checkout');
+            dispatch(toggleCartHidden())
+            }
+        }> GO TO CHECKOUT</CustomButton>
     </div>
 )
 
@@ -28,4 +34,4 @@ const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 })
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
