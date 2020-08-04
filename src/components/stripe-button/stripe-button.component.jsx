1e9363d0426317b3eb733134721f99aa04c20798
caglarorhan <caglaror@gmail.com>
2020-08-03 23:58:48 -0500

    before Stripe implementation

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const StripeCheckoutButton = ({price})=>{
+    const priceForStripe = price*100;
+    const publishableKey = 'pk_test_51HBtHKBE2crNxvbsUeO80xeQZd6TquBeofWBFmIDZchjcJkC1GgXWP2bIStwhnFlcTLk9A3d8edZ8bBCIrlNPjP800jX6zBxhm';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment succesful')
+    }
+
+    return (
+        <StripeCheckout
+        label='Pay Now'
+        name='EMarket'
+        billingAddress
+        shippingAddress
+        image = 'https://svgshare.com/i/CUz.svg'
+        descriptin={`Yout total us $${price}`}
+        amount={priceForStripe}
+        panelLabel = 'Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
