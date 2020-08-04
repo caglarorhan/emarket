2a0fbaf3f7dae6bec163f20b419078ea93fc2695
caglarorhan <caglaror@gmail.com>
2020-08-04 15:03:13 -0500

    Firebase config added and open for public

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAF1wHFZ5SWJSwNstmuyloCWqCwf1COBNQ",
+    authDomain: "emarket-0001.firebaseapp.com",
+    databaseURL: "https://emarket-0001.firebaseio.com",
+    projectId: "emarket-0001",
+    storageBucket: "emarket-0001.appspot.com",
+    messagingSenderId: "567346034911",
+    appId: "1:567346034911:web:4bbc2ba3d317de3f04916a",
+    measurementId: "G-DR6YD8388V"
+};
+export {config};
