53b810924245bf3c0f1060ffa8ca1202cae9e15c
caglarorhan <caglaror@gmail.com>
2020-08-24 04:37:48 -0500

    Begin to refactor from thunk to saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware} from "redux";
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import {fetchCollectionsStart} from "./shop/shop.saga";
 
 import rootReducer from './root-reducer';
 
+const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 if(process.env.NODE_ENV==='development'){
  middlewares.push(logger);
@@ -14,6 +17,7 @@ if(process.env.NODE_ENV==='development'){
 
 
  const store = createStore(rootReducer, applyMiddleware(...middlewares))
+ sagaMiddleware.run(fetchCollectionsStart)
  const persistor = persistStore(store);
 
 
