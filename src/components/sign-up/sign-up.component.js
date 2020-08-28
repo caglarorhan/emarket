ceceb20690fd43e916ee8baf2005957fc8fd45d9
caglarorhan <caglaror@gmail.com>
2020-08-28 12:10:20 -0500

    Saga refactoring done

--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -1,11 +1,10 @@
 import React from "react";
-
+import {connect} from "react-redux";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-
-import {auth, createUserProfileDocument} from "../../firebase/firebase.utils";
-
+import {signUpStart} from '../../redux/user/user.action';
 import './sign-up.style.scss';
+import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
 
 class SignUp extends React.Component{
     constructor() {
@@ -20,23 +19,12 @@ class SignUp extends React.Component{
 
     handleSubmit = async (e)=>{
         e.preventDefault();
+        const {signUpStart} = this.props;
         const {displayName, email, password, confirmPassword} = this.state;
         if(password!==confirmPassword){
             alert("Passwords don't match!");
-            return;
         }
-        try{
-            const {user} = await auth.createUserWithEmailAndPassword(email, password);
-            await createUserProfileDocument(user, {displayName});
-            this.setState({
-                displayName:'',
-                email:'',
-                password:'',
-                confirmPassword:''
-            })
-        }catch(error){
-            console.log(error);
-        }
+        signUpStart({displayName, email, password});
     }
 
 handleChange = e =>{
@@ -89,4 +77,8 @@ handleChange = e =>{
     }
 }
 
-export default SignUp;
+const mapDispatchToProps = dispatch => ({
+    signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
+})
+
+export default connect(null,mapStateToProps)(SignUp);
